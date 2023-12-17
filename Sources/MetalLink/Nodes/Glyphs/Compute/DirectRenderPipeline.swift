7f420f874aa211bcc50affa9b3d3998e6a66171e
Ivan Lugo - XCode <lugo@ischemaview.com>
2023-12-17 16:15:30 -0600

    - So here's this giant direct data renderer

--- Sources/MetalLink/Nodes/Glyphs/Compute/DirectRenderPipeline.swift
+++ Sources/MetalLink/Nodes/Glyphs/Compute/DirectRenderPipeline.swift
@@ -7,6 +7,7 @@ import Foundation
 import MetalKit
 import MetalLinkHeaders
 import BitHandling
+import Combine
 
 public struct FileWatchRenderer: MetalLinkReader {
     public let link: MetalLink
@@ -27,14 +28,15 @@ public struct FileWatchRenderer: MetalLinkReader {
     }
 
     public func regenerateCollectionForSource() throws -> GlyphCollection {
+        let encodeResult = try compute.executeSingleWithAtlas(
+            source: sourceUrl,
+            atlas: atlas
+        )
         
-        let buffer = try readUrlIntoBuffer()
-        let encodeResult = try encodeLayout(for: buffer)
-        try encodeBlit(for: encodeResult)
-        try rebuildCollection(for: encodeResult)
         switch encodeResult.collection {
         case .built(let result):
             return result
+            
         case .notBuilt:
             print("""
             XXX - Encoding pipeline failed for url: \(sourceUrl), returning default empty collection. Expect bad things.
@@ -44,105 +46,76 @@ public struct FileWatchRenderer: MetalLinkReader {
     }
 }
 
-private extension FileWatchRenderer {
-    // MARK: [ 1 ] - Read source into buffer
-    func readUrlIntoBuffer() throws -> MTLBuffer {
-        let data: Data
-        if sourceUrl.isDirectory {
-            data = "<directory-url:\(sourceUrl)>".data(using: .utf8)!
-        } else {
-            data = try Data(contentsOf: sourceUrl, options: [.alwaysMapped])
-        }
-        
-        let buffer = try compute.makeInputBuffer(data)
-        buffer.label = "Input grapheme \(sourceUrl.lastPathComponent)"
-        return buffer
-    }
+public class DataStreamRenderer: MetalLinkReader {
+    public typealias DataStream = AnyPublisher<Data, Never>
+    public typealias CollectionStream = AnyPublisher<GlyphCollection, Never>
     
-    // MARK: [ 2 ] - Read source into buffer
-    func encodeLayout(for buffer: MTLBuffer) throws -> EncodeResult {
-        guard let commandBuffer = commandQueue.makeCommandBuffer()
-        else { throw ComputeError.commandBufferCreationFailed }
-        
-        // Setup the first atlas + layout encoder
-        let (
-            outputUTF32ConversionBuffer,
-            characterCountBuffer,
-            computeCommandEncoder
-        ) = try compute.setupAtlasLayoutCommandEncoder(
-            for: buffer,
-            in: commandBuffer,
-            atlasBuffer: atlas.currentBuffer
-        )
-        
-        // Setup the result (this is weird but it made sense at the time)
-        let mappedLayout = EncodeResult(
-            sourceURL: sourceUrl,
-            outputBuffer: outputUTF32ConversionBuffer,
-            characterCountBuffer: characterCountBuffer,
-            sourceEncoder: computeCommandEncoder
-        )
-        
-        // Commit our buffer and schedule it
-        commandBuffer.commit()
-        commandBuffer.waitUntilCompleted()
-        
-        return mappedLayout
-    }
+    public let link: MetalLink
+    public var atlas: MetalLinkAtlas
+    public let compute: ConvertCompute
+    public let name: String
     
-    func encodeBlit(for result: EncodeResult) throws {
-        guard let commandBuffer = commandQueue.makeCommandBuffer()
-        else { throw ComputeError.commandBufferCreationFailed }
-        
-        switch result.blitEncoder {
-        case .notSet:
-            // Create a new instance state to blit our glyph data into
-            guard result.finalCount > 0 else {
-                print("-- (Couldn't map; empty final count for: \(result.sourceURL)")
-                return
+    public let sourceStream: DataStream
+    public let collectionStream: CollectionStream
+    
+    private var token: Any?
+    
+    public init(
+        link: MetalLink,
+        atlas: MetalLinkAtlas,
+        compute: ConvertCompute,
+        dataStream: DataStream,
+        name: String
+    ) {
+        self.link = link
+        self.atlas = atlas
+        self.compute = compute
+        self.name = name
+        self.sourceStream = dataStream
+        self.collectionStream = dataStream
+            .receive(on: DispatchQueue.global())
+            .compactMap { data in
+                do {
+                    return try Self.regenerateCollection(
+                        name: name,
+                        for: data,
+                        compute: compute,
+                        atlas: atlas,
+                        link: link
+                    )
+                } catch {
+                    print(error)
+                    return nil
+                }
             }
-            
-            let newState = try InstanceState(
-                link: link,
-                bufferSize: Int(Float(result.finalCount) * 1.5),
-                instanceBuilder: atlas.nodeCache.create
-            )
-            
-            // Setup the blitter which maps the unicode magic to the render magic
-            let blitEncoder = try compute.setupCopyBlitCommandEncoder(
-                for: result.outputBuffer,
-                targeting: newState,
-                expectedCharacterCount: result.finalCount,
-                in: commandBuffer
-            )
-            result.blitEncoder = .set(blitEncoder, newState)
-
-        case .set(_, _):
-            print("""
-            ----
-            ---- Invalid encode state for: \(sourceUrl)
-            ----
-            """)
-        }
-        
-        commandBuffer.commit()
-        commandBuffer.waitUntilCompleted()
+            .receive(on: DispatchQueue.main)
+            .share()
+            .eraseToAnyPublisher()
     }
-    
-    func rebuildCollection(for result: EncodeResult) throws {
-        switch result.blitEncoder {
-        case .set(_, let state):
-            state.constants.currentEndIndex = Int(result.finalCount)
-            let collection = try GlyphCollection(
-                link: link,
-                linkAtlas: atlas,
-                instanceState: state
-            )
-            collection.resetCollectionState()
-            result.collection = .built(collection)
+
+    private static func regenerateCollection(
+        name: String,
+        for data: Data,
+        compute: ConvertCompute,
+        atlas: MetalLinkAtlas,
+        link: MetalLink
+    ) throws -> GlyphCollection {
+        let encodeResult = try compute.executeDataWithAtlas(
+            name: name,
+            source: data,
+            atlas: atlas
+        )
+        
+        switch encodeResult.collection {
+        case .built(let result):
+            return result
             
-        case .notSet:
-            break
+        case .notBuilt:
+            print("""
+            XXX - Encoding pipeline failed for name: \(name)
+            XXX   Returning default empty collection. Expect bad things.
+            """)
+            return try GlyphCollection(link: link, linkAtlas: atlas)
         }
     }
 }
