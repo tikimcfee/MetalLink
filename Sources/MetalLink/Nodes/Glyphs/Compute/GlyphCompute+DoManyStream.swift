decc0393e65249839d5b321f1871fe87c61f8509
Ivan Lugo <ivan.lugo.dev@pm.me>
2024-11-16 08:41:37 -0600

    - One more for good measure because streams and concurrency... I guess? Walls were missing. I dunno.

--- Sources/MetalLink/Nodes/Glyphs/Compute/GlyphCompute+DoManyStream.swift
+++ Sources/MetalLink/Nodes/Glyphs/Compute/GlyphCompute+DoManyStream.swift
@@ -15,6 +15,154 @@ import Combine
 // MARK: -- [Many]
 
 public extension ConvertCompute {
+    func executeManyWithAtlas_Conc(
+        in source: URL,
+        atlas: MetalLinkAtlas
+    ) -> EncodeResult? {
+        guard let buffer = makeBuffer() else { return nil }
+        guard let result = doEncode(with: buffer) else { return nil }
+        guard let blit = doBlit(with: result) else { return nil }
+        guard let rebuild = doRebuild(on: blit) else { return nil }
+        return rebuild
+        
+        func makeBuffer() -> MTLBuffer? {
+            do {
+                var data: Data
+                if source.isSupportedFileType {
+                    data = try Data(
+                        contentsOf: source,
+                        options: [.alwaysMapped]
+                    )
+                    
+                    if data.count == 0 {
+                        data = "<empty-file>".data(using: .utf8)!
+                    }
+                } else {
+                    data = """
+                Unsupported file type
+                \(source.path())
+                """.data(using: .utf8)!
+                }
+                
+                let maxSize = Int(1024.0 * 1024.0 * 2)
+                if data.count > maxSize {
+                    print("Prefixing large file from full render: \(source.pathComponents.suffix(2)) - \(data.count.megabytes)")
+                    data = data.prefix(maxSize)
+                    print("New size: \(source.pathComponents.suffix(2)) - \(data.count.megabytes)")
+                }
+                
+                let buffer = try self.makeInputBuffer(data)
+                buffer.label = "Input graphemes \(source.lastPathComponent)"
+                print("Input buffer length: \(source.pathComponents.suffix(2)) -- \(buffer.length)")
+                
+                return buffer
+            } catch {
+                print(error)
+                return nil
+            }
+        }
+        
+        func doEncode(with buffer: MTLBuffer) -> EncodeResult? {
+            do {
+                // Setup the first atlas + layout encoder
+                guard let commandBuffer = self.commandQueue.makeCommandBuffer()
+                else { throw ComputeError.startupFailure }
+                
+                let (
+                    outputUTF32ConversionBuffer,
+                    characterCountBuffer,
+                    computeCommandEncoder
+                ) = try self.setupAtlasLayoutCommandEncoder(
+                    for: buffer,
+                    in: commandBuffer,
+                    atlasBuffer: atlas.currentBuffer
+                )
+                
+                computeCommandEncoder.insertDebugSignpost("[SG] Created Atlas Layout Encoder - \(source.lastPathComponent)")
+                
+                // Setup the result (this is weird but it made sense at the time)
+                let mappedLayout = EncodeResult(
+                    sourceURL: source,
+                    outputBuffer: outputUTF32ConversionBuffer,
+                    characterCountBuffer: characterCountBuffer,
+                    sourceEncoder: computeCommandEncoder
+                )
+                
+                commandBuffer.commit()
+                commandBuffer.waitUntilCompleted()
+                
+                // We're off to the races <3
+                return mappedLayout
+            } catch {
+                print(error)
+                return nil
+            }
+        }
+        
+        func doBlit(with result: EncodeResult) -> EncodeResult? {
+            do {
+                guard let commandBuffer = self.commandQueue.makeCommandBuffer()
+                else { throw ComputeError.startupFailure }
+                
+                switch result.blitEncoder {
+                case .notSet:
+                    // Create a new instance state to blit our glyph data into
+                    guard result.finalCount > 0 else {
+                        print("-- (Couldn't map; empty final count for: \(result.sourceURL)")
+                        return nil
+                    }
+                    
+                    let newState = try InstanceState(
+                        link: self.link,
+                        bufferSize: Int(Float(result.finalCount) * 1.5),
+                        instanceBuilder: atlas.nodeCache.create
+                    )
+                    
+                    // Setup the blitter which maps the unicode magic to the render magic
+                    let blitEncoder = try self.setupCopyBlitCommandEncoder(
+                        for: result.outputBuffer,
+                        targeting: newState,
+                        expectedCharacterCount: result.finalCount,
+                        in: commandBuffer
+                    )
+                    result.blitEncoder = .set(blitEncoder, newState)
+                    
+                    commandBuffer.commit()
+                    commandBuffer.waitUntilCompleted()
+                    
+                    return result
+                case .set(_, _):
+                    fatalError("this.. how!?")
+                }
+            } catch {
+                print(error)
+                return nil
+            }
+        }
+        
+        func doRebuild(on result: EncodeResult) -> EncodeResult? {
+            switch result.blitEncoder {
+            case .notSet:
+                return nil
+                
+            case .set(_, let state):
+                do {
+                    state.constants.currentEndIndex = Int(result.finalCount)
+                    let collection = try GlyphCollection(
+                        link: self.link,
+                        linkAtlas: atlas,
+                        instanceState: state
+                    )
+                    collection.resetCollectionState()
+                    result.collection = .built(collection)
+                    return result
+                } catch {
+                    fatalError("-- What happen? Someone set us up a bomb?\n\(error)")
+                }
+            }
+        }
+    }
+    
     func executeManyWithAtlas_Stream(
         atlas: MetalLinkAtlas
     ) -> (
